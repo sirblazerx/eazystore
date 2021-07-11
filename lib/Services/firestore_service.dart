47097faf9ee085a18d34399e842d1f6abdb32805
Saiful <sirblazerx@gmail.com>
2021-07-11 15:00:13 +0800

    Added remaining models

--- lib/Services/firestore_service.dart
+++ lib/Services/firestore_service.dart
@@ -1,9 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirestoreService {
+  
   FirebaseFirestore _db = FirebaseFirestore.instance;
 
-//Get Data
+
+
+// Get Menu Data
 
 // Create Data
 
